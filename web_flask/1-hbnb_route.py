63c1c420c96467fbe91d9a6b4cc08f179bedf642
EXTRADEEN <dinumira@gmail.com>
2023-04-04 12:13:46 +0200

    PEP8 validation fixed

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,12 +8,16 @@ app = Flask(__name__)
 
 
 """ Route for the homepage """
+
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Returns Hello HBNB """
     return 'Hello HBNB!'
 
 """ Route for the /hbnb """
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Returns HBNB """
