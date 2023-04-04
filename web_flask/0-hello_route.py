7d7d48f30752e75ab384294942bf9740b1f1a452
EXTRADEEN <dinumira@gmail.com>
2023-04-04 11:30:13 +0200

    script that starts a Flask web aplication

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,6 +5,7 @@ from flask import Flask
 """ Create a instance """
 app = Flask(__name__)
 
+
 """ Route for the homepage """
 @app.route('/', strict_slashes=False)
 def home():
@@ -12,5 +13,5 @@ def home():
 
 
 if __name__ == '__main__':
-    # Start the aplication on the port
+    """ Start the aplication on the port """
     app.run(host='0.0.0.0', port=5000)
